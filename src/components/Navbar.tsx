63a5db92c278c5f598cdf94e44fc8ef5e87627ef
Thant Zin Win <thant.zinwin@yomabank.com>
2023-01-23 13:28:39 +0630

    implements navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,56 @@
-import { Stack, Box, Typography } from "@mui/material";
+import { Stack, Box, Typography,Divider } from "@mui/material";
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <Stack
       direction={"row"}
-      p={2}
+      py={2}
+      px={4}
       alignItems={"center"}
       sx={{
         justifyContent: "space-between",
         background: "white",
         borderBottom: "0.1px solid gray",
       }}
-      height={"150px"}
+      height={"110px"}
     >
       <Link to={"/"}>
+        <Box width={"140px"}>
         <img
           src="https://res.cloudinary.com/dhprtrwtd/image/upload/v1648056557/WG_2022_reskin.svg"
           alt="logo"
-          width={"150px"}
+          width={"100%"}
         />
+        </Box>
       </Link>
-      <Box>
-        <Typography>Auctions</Typography>
+      <Box display={"flex"} gap={8}>
+        <Link to={"/auctions"} color="">
+        <Typography>Auctions</Typography>      
+        </Link>
+        <Typography display={"flex"} alignItems="center">Auctions Categories
+          <ExpandMoreOutlinedIcon/>
+        </Typography>
+
+        <Link to={"/about"}>
+        <Typography>About Us</Typography>
+        </Link>
+
+        <Box sx={{display:"flex",gap:2}}>
+          <Link to={"/signup"}>
+            <Typography>
+            Sign Up
+
+            </Typography>
+          </Link>
+        <Divider orientation="vertical" flexItem sx={{border:1,borderColor:'error.dark'}}/>
+
+        <Link to={"/account"}>
+        <Typography>Account</Typography>
+        </Link>
+
+        </Box>       
       </Box>
     </Stack>
   );
