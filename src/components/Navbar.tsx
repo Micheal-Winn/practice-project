ea88986a1b029dd66052e632c131d7355cea353b
thantzinwin <thant.zin.winnnn@gmail.com>
2023-01-28 19:10:26 +0630

    refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Stack, Box, Typography,Divider } from "@mui/material";
-import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import { Stack, Box, Typography, Divider } from "@mui/material";
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
@@ -18,39 +18,40 @@ export default function Navbar() {
     >
       <Link to={"/"}>
         <Box width={"140px"}>
-        <img
-          src="https://res.cloudinary.com/dhprtrwtd/image/upload/v1648056557/WG_2022_reskin.svg"
-          alt="logo"
-          width={"100%"}
-        />
+          <img
+            src="https://res.cloudinary.com/dhprtrwtd/image/upload/v1648056557/WG_2022_reskin.svg"
+            alt="logo"
+            width={"100%"}
+          />
         </Box>
       </Link>
       <Box display={"flex"} gap={8}>
         <Link to={"/auctions"} color="">
-        <Typography>Auctions</Typography>      
+          <Typography>Auctions</Typography>
         </Link>
-        <Typography display={"flex"} alignItems="center">Auctions Categories
-          <ExpandMoreOutlinedIcon/>
+        <Typography display={"flex"} alignItems="center">
+          Auctions Categories
+          <ExpandMoreOutlinedIcon />
         </Typography>
 
         <Link to={"/about"}>
-        <Typography>About Us</Typography>
+          <Typography>About Us</Typography>
         </Link>
 
-        <Box sx={{display:"flex",gap:2}}>
+        <Box sx={{ display: "flex", gap: 2 }}>
           <Link to={"/signup"}>
-            <Typography>
-            Sign Up
-
-            </Typography>
+            <Typography>Sign Up</Typography>
           </Link>
-        <Divider orientation="vertical" flexItem sx={{border:1,borderColor:'error.dark'}}/>
-
-        <Link to={"/account"}>
-        <Typography>Account</Typography>
-        </Link>
-      console.log("helo")
-        </Box>       
+          <Divider
+            orientation="vertical"
+            flexItem
+            sx={{ border: 1, borderColor: "error.dark" }}
+          />
+          <Link to={"/account"}>
+            <Typography>Account</Typography>
+          </Link>
+          console.log("helo")
+        </Box>
       </Box>
     </Stack>
   );
