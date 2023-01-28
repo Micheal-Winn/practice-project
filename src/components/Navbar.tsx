fc332afe4ca07d9f5b36b8d6e4a38a4f9c73a6c6
thantzinwin <thant.zin.winnnn@gmail.com>
2023-01-28 20:44:11 +0630

    progress stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,58 +1,77 @@
-import { Stack, Box, Typography, Divider } from "@mui/material";
+import { Stack, Box, Typography, Divider, Container } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
 import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <Stack
-      direction={"row"}
-      py={2}
-      px={4}
-      alignItems={"center"}
-      sx={{
-        justifyContent: "space-between",
-        background: "white",
-        borderBottom: "0.1px solid gray",
-      }}
-      height={"110px"}
-    >
-      <Link to={"/"}>
-        <Box width={"140px"}>
-          <img
-            src="https://res.cloudinary.com/dhprtrwtd/image/upload/v1648056557/WG_2022_reskin.svg"
-            alt="logo"
-            width={"100%"}
-          />
-        </Box>
-      </Link>
-      <Box display={"flex"} gap={8}>
-        <Link to={"/auctions"} color="">
-          <Typography>Auctions</Typography>
+    <Box display={"flex"} flexDirection="column" boxShadow={"0 3px 2px #F5F5F5"}>
+      <Stack
+        direction={"row"}
+        py={2}
+        px={4}
+        alignItems={"center"}
+        sx={{
+          justifyContent: "space-between",
+          background: "white",
+        }}
+        height={"110px"}
+      >
+        <Link to={"/"}>
+          <Box width={"140px"}>
+            <img
+              src="https://res.cloudinary.com/dhprtrwtd/image/upload/v1648056557/WG_2022_reskin.svg"
+              alt="logo"
+              width={"100%"}
+            />
+          </Box>
         </Link>
-        <Typography display={"flex"} alignItems="center">
-          Auctions Categories
-          <ExpandMoreOutlinedIcon />
-        </Typography>
-
-        <Link to={"/about"}>
-          <Typography>About Us</Typography>
-        </Link>
-
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <Link to={"/signup"}>
-            <Typography>Sign Up</Typography>
+        <Box display={"flex"} gap={8}>
+          <Link
+            to={"/auctions"}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <Typography>Auctions</Typography>
           </Link>
-          <Divider
-            orientation="vertical"
-            flexItem
-            sx={{ border: 1, borderColor: "error.dark" }}
-          />
-          <Link to={"/account"}>
-            <Typography>Account</Typography>
+          <Typography display={"flex"} alignItems="center">
+            Auctions Categories
+            <ExpandMoreOutlinedIcon />
+          </Typography>
+
+          <Link
+            to={"/about"}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <Typography>About Us</Typography>
           </Link>
-          console.log("helo")
+
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Link
+              to={"/signup"}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <Typography>Sign Up</Typography>
+            </Link>
+            <Divider
+              orientation="vertical"
+              flexItem
+              sx={{ border: 1, borderColor: "error.dark" }}
+            />
+            <Link
+              to={"/account"}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <Typography>Account</Typography>
+            </Link>
+          </Box>
         </Box>
-      </Box>
-    </Stack>
+      </Stack>
+
+        <Container>
+          
+          
+        </Container>
+      
+    </Box>
   );
 }
